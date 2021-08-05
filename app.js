7167bc778d680ad8bb0aabfd4da8e11fa457fad1
Nasib Mammadov <bduti09@gmail.com>
2021-08-06 00:46:53 +0400

    fixed

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3000;
+
+
+
+
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+})
+
+app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`))
---
+++ client/fontend.js
@@ -0,0 +1,36 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      form: {
+        name: '',
+        value: ''
+      },
+      contacts: [
+        {id: 1, name: 'Насиб Мамедов', value: '+994-50-701-00-91', marked: false}
+      ]
+    }
+  },
+  computed: {
+    canCreate() {
+      return this.form.value.trim() && this.form.name.trim()
+    }
+  },
+  methods: {
+    createContact() {
+      const {...contact} = this.form
+      this.contacts.push({...contact, id: Date.now()})
+      
+      this.form.name = this.form.value = '';
+    },
+    markContact(id) {
+      const contact = this.contacts.find(c => c.id === id)
+      contact.marked = true;
+    },
+    removeContact(id) {
+
+    }
+  }
+})
